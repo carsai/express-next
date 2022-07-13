7b38e7c20ce9a54af23b807da2e36657f1ffd4b9
Carsai <psgh1989@gmail.com>
2022-07-13 23:48:00 +0200

    routing

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function() {
     return(
         <div>
             <h1>Home</h1>
+            <Link href="/mixto">
+                <button>Ir a mixto</button>
+            </Link>
         </div>
     );
 }
